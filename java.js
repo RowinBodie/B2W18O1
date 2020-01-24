33650bfddd319dac0ce4410fa0b6e14264edb965
Rowin Bodegom <99056991@mydavinci.nl>
2020-01-24 10:00:01 +0100

    uitklapmenu werkt
    ben nu bezig met het overzetten naar pc

--- java.js
+++ java.js
@@ -1,6 +1,20 @@
-function view0(){
-    alert("hi");
-}
-function view1(){
-    alert("bye");
+var knop = document.getElementById('bars0');
+knop.setAttribute('onclick','showMenu(0)');
+
+var knop = document.getElementById('bars1');
+knop.setAttribute('onclick','showMenu(1)');
+
+function showMenu(number){
+    if(number == 0){
+        var menu = document.getElementById('menu0');
+    }
+    else if(number == 1){
+        var menu = document.getElementById('menu1');
+    }
+    if(menu.style.height == "0px" || menu.style.height == ""){
+        menu.style.height = "auto";
+    }
+    else{
+        menu.style.height = "0px";
+    }
 }
